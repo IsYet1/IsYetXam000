9b325ad3a61b85bba1f9b98c47a882aa26f773ca
IsYet1 <Don@IsYet.com>
2016-08-21 20:21:03 -0500

    Initial checkin
    No changes. Copied the files. Added the .ignore file.

---
+++ IsYetXam000/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IsYetXam000
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services and gateway URLs.
+		public static string ApplicationURL = @"https://isyetxam000.azurewebsites.net";
+	}
+}
+
